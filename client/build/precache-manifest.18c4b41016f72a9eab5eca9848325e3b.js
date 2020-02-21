39ed1b9328740c5f80ea082e76df41173f89b229
evzpav <evzpav@gmail.com>
2020-02-21 00:02:04 -0300

    build folder

---
+++ client/build/precache-manifest.18c4b41016f72a9eab5eca9848325e3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fec2ae15267a7051143557b396e4d2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e932c36b16c737c8403d",
+    "url": "/static/css/main.f5c34bbc.chunk.css"
+  },
+  {
+    "revision": "40c291aea202e2afa2ec",
+    "url": "/static/js/2.c1e50069.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c1e50069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e932c36b16c737c8403d",
+    "url": "/static/js/main.f51d77fb.chunk.js"
+  },
+  {
+    "revision": "8d891c7f46cef14bbc18",
+    "url": "/static/js/runtime-main.eb44c8d0.js"
+  },
+  {
+    "revision": "d00e94dde1fa21f5e6e862295cd98f88",
+    "url": "/static/media/knight.d00e94dd.svg"
+  }
+]);
